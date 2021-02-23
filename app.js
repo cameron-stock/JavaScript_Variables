bf0169304aae95bc27a50a178cd7365ae7d7b080
Cameron Stock <clstock33@gmail.com>
2021-02-22 20:08:29 -0600

    init commit

--- app.js
+++ app.js
@@ -4,7 +4,17 @@ console.log(
 );
 
 // Exercise 1
+const firstName = "Cameron";
+let lastName = "Stock";
+var age = 28;
 
 // Exercise 2
+let fullName = firstName + " " + lastName;
+console.log(fullName);
 
 // Exercise 3
+let city = "Dallas";
+let passTime = "Coding";
+
+let myStory = `Hello There, I'm ${fullName}. I live in ${city}. I enjoy being a jedi and ${passTime}.`;
+console.log(myStory);
